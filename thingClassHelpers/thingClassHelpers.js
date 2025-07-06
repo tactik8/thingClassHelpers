a2ea05095ff67eb19b829dae47a9deec76d786c8
tactik8 <48621231+tactik8@users.noreply.github.com>
2025-07-06 19:02:14 +0000

    go

--- thingClassHelpers/thingClassHelpers.js
+++ thingClassHelpers/thingClassHelpers.js
@@ -1,13 +1,15 @@
 
 import { Thing } from './src/thing/thing.models.js';
-//import { ProgramMembership } from './src/ProgramMembershipClass.js';
-//import { Role } from './src/RoleClass.js';
+import { ProgramMembership } from './src/ProgramMembershipClass.js';
+import { Role } from './src/RoleClass.js';
 import { Action } from './src/action/action.models.js';
+import { ProgramMembershipTier } from './src/MemberProgramTierClass.js';
 
 
 export const thingClassHelpers = {
     Thing: Thing,
-   // ProgramMembership: ProgramMembership,
-    //Role: Role,
+    ProgramMembership: ProgramMembership,
+    ProgramMembershipTier: ProgramMembershipTier,
+    Role: Role,
     Action: Action
 }
